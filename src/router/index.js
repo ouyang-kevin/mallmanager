8256b838ad4777918c5c39555c1a72b6786c008e
ouyang-kevin <1572620397@qq.com>
2021-01-10 00:00:43 +0800

    新建登录分支新建登录组件配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// import Login from '../components/login/login.vue'
+// @ -> src
+import Login from '@/components/login/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
