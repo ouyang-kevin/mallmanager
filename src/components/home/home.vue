23c51f384e7c84efa3e8233db2263e07bf510b67
ouyang-kevin <1572620397@qq.com>
2021-01-11 15:39:36 +0800

    引入导航组件和头部组件

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,11 +1,133 @@
 <template>
-  <div>
-    home
-  </div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">
+            <img src="@/assets/img/logo.png" alt="无法显示图片" />
+          </div>
+        </el-col>
+        <el-col :span="18" class="middle">
+          <h3>电商后台管理系统</h3>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple"></div>
+          <a class="loginout" href="#">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">
+        <el-menu
+        unique-opened = "true"
+        >
+          <!-- 1 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                用户列表
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="2-1">
+                <i class="el-icon-menu"></i>
+                角色列表
+              </el-menu-item>
+            </el-menu-item-group>
+              <el-menu-item index="2-2">
+                <i class="el-icon-menu"></i>
+                权限列表
+              </el-menu-item>
+          </el-submenu>
+          <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                商品列表
+              </el-menu-item>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                分类参数
+              </el-menu-item>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                商品分类
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <!-- 4 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                订单列表
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                数据报表
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
-export default {
-}
+export default {};
 </script>
 <style>
+.container {
+  height: 100%;
+}
+.header {
+  background-color: #b3c0d1;
+}
+.aside {
+  background-color: #d3dce6;
+}
+.main {
+  background-color: #e9eef3;
+  height: 100%;
+}
+
+/* 头部样式 */
+.middle {
+  text-align: center;
+}
+.loginout {
+  text-decoration: none;
+  line-height: 60px;
+}
 </style>
